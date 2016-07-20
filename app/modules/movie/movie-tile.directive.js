2a2f9bf00efb99943b2a55cd0e542a0d0851bff3
Greg Goforth <greg.goforth@gmail.com>
2016-07-19 19:35:51 -0700

    adds movie-tile directive scoping, and updates to gulpfile

--- app/modules/movie/movie-tile.directive.js
+++ app/modules/movie/movie-tile.directive.js
@@ -6,10 +6,20 @@
     .directive('movieTile', movieTile);
 
   function movieTile() {
+    function controller() {}
+    
     return {
       restrict: 'E',
+      controller: controller,
+      controllerAs: 'movietile',
+      bindToController: true,
+      scope: {
+        movie: '=',
+        selectedMovie: '='
+      },
       templateUrl: 'build/partials/movie/movie-tile.html'
     };
   }
 
 }());
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,76 @@
 ﻿var gulp = require('gulp'),
-    //less = require('gulp-less'),
-    autoprefixer = require('gulp-autoprefixer'),
-    plumber = require('gulp-plumber'),
-    uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    ngAnnotate = require('gulp-ng-annotate'),
-    watch = require('gulp-watch'),
-    //gulpif = require('gulp-if'),
-    livereload = require('gulp-livereload'),
-    serve = require('gulp-serve');
+//less = require('gulp-less'),
+  autoprefixer = require('gulp-autoprefixer'),
+  plumber = require('gulp-plumber'),
+  uglify = require('gulp-uglify'),
+  htmlify = require('gulp-angular-htmlify'),
+  jshint = require('gulp-jshint'),
+  iife = require('gulp-iife'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  ngAnnotate = require('gulp-ng-annotate'),
+  watch = require('gulp-watch'),
+//gulpif = require('gulp-if'),
+  livereload = require('gulp-livereload'),
+  serve = require('gulp-serve');
 
 gulp.task('js-deps', function () {
-    gulp.src([
-        './bower_components/jquery/dist/jquery.js',
-        './bower_components/lodash/lodash.js',
-        './bower_components/moment/moment.js',
-        './bower_components/angular/angular.js',
-        './bower_components/angular-ui-router/release/angular-ui-router.js',
-        './bower_components/angular-bootstrap/ui-bootstrap-tpls.js'
-    ])
-        .pipe(concat('deps.js'))
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./build/js'));
+  gulp.src([
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/lodash/lodash.js',
+    './bower_components/moment/moment.js',
+    './bower_components/angular/angular.js',
+    './bower_components/angular-ui-router/release/angular-ui-router.js',
+    './bower_components/angular-bootstrap/ui-bootstrap-tpls.js'
+  ])
+    .pipe(concat('deps.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('partials', function () {
-    gulp.src('./app/modules/**/*.html')
-        .pipe(gulp.dest('./build/partials'))
-        .pipe(livereload());
+  gulp.src('./app/modules/**/*.html')
+    .pipe(htmlify({
+      customPrefixes: ['ui-']
+    }))
+    .pipe(gulp.dest('./build/partials'))
+    .pipe(livereload());
 });
 
 gulp.task('css-deps', function () {
-    gulp.src([
-        "./bower_components/bootstrap/dist/css/bootstrap.min.css",
-        "./bower_components/font-awesome/css/font-awesome.min.css"
-    ])
-        .pipe(concat('css-deps.css'))
-        .pipe(gulp.dest('./build/css'));
+  gulp.src([
+    "./bower_components/bootstrap/dist/css/bootstrap.min.css",
+    "./bower_components/font-awesome/css/font-awesome.min.css"
+  ])
+    .pipe(concat('css-deps.css'))
+    .pipe(gulp.dest('./build/css'));
 
-    gulp.src('./bower_components/font-awesome/fonts/*')
-        .pipe(gulp.dest('./build/fonts'));
+  gulp.src('./bower_components/font-awesome/fonts/*')
+    .pipe(gulp.dest('./build/fonts'));
 });
 
 gulp.task('js', function () {
-    var baseDir = __dirname + '/app/modules',
-        outputDir = __dirname + '/build/js',
-        outputFilename = 'app.js';
+  var baseDir = __dirname + '/app/modules',
+    outputDir = __dirname + '/build/js',
+    outputFilename = 'app.js';
 
-    gulp.src([
-        baseDir + "/*module.js",
-        baseDir + "/**/*module.js",
-        baseDir + "/**/*.js"
-    ])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        //.pipe(sourcemaps.init())
-        .pipe(concat(outputFilename))
-        .pipe(ngAnnotate())
-        //.pipe(uglify())
-        //.pipe(sourcemaps.write())
-        .pipe(gulp.dest(outputDir))
-        .pipe(livereload());
+  gulp.src([
+    baseDir + "/*module.js",
+    baseDir + "/**/*module.js",
+    baseDir + "/**/*.js"
+  ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(iife())
+    //.pipe(sourcemaps.init())
+    .pipe(concat(outputFilename))
+    .pipe(ngAnnotate())
+    //.pipe(uglify())
+    //.pipe(sourcemaps.write())
+    .pipe(gulp.dest(outputDir))
+    .pipe(livereload());
 });
 
 // gulp.task('less', function () {
@@ -79,25 +85,25 @@ gulp.task('js', function () {
 // });
 
 gulp.task('data', function () {
-    gulp.src('./app/data/movies.json')
-        .pipe(gulp.dest('./build/data/'));
+  gulp.src('./app/data/movies.json')
+    .pipe(gulp.dest('./build/data/'));
 });
 
 gulp.task('serve', serve('.'));
 
 gulp.task('watch', function () {
-    livereload.listen({ port: 35730 });
-    watch(['./app/modules/*.js', './app/modules/**/*.js'], function () {
-        gulp.start('js');
-    });
+  livereload.listen();
+  watch(['./app/modules/*.js', './app/modules/**/*.js'], function () {
+    gulp.start('js');
+  });
 
-    watch('./app/less/*.less', function () {
-        gulp.start('less');
-    });
+  watch('./app/less/*.less', function () {
+    gulp.start('less');
+  });
 
-    watch(['./app/modules/*.html', './app/modules/**/*.html'], function () {
-        gulp.start('partials');
-    });
+  watch(['./app/modules/*.html', './app/modules/**/*.html'], function () {
+    gulp.start('partials');
+  });
 });
 
 gulp.task('default', ['js-deps', 'partials', 'data', 'css-deps', 'js', 'watch', 'serve']);
