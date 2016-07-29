3e13ff30336d90581ce4ca822f42f49440dfb878
Greg Goforth <greg.goforth@gmail.com>
2016-07-28 20:47:54 -0700

    add comments

--- app/modules/movie/movie.controller.js
+++ app/modules/movie/movie.controller.js
@@ -5,6 +5,7 @@ function MovieCtrl(movie, storage) {
   var vm = this;
   vm.movie = movie; 
   vm.addComment = addComment;
+  vm.trashComment = trashComment;
   vm.emailPattern = /[a-z]+@[a-z]+\.(com|edu|net|io)/;
 
   /**
@@ -22,5 +23,15 @@ function MovieCtrl(movie, storage) {
     storage.set(vm.movie.id + '.comment', vm.movie.comments);
     
     vm.comment = ''; 
+  }
+
+  /**
+   * Remove a comment.
+   * 
+   * @param comment
+   */
+  function trashComment(comment) {
+    _.remove(vm.movie.comments, comment);
+    storage.set(vm.movie.id + '.comment', vm.movie.comments);
   }
 }
